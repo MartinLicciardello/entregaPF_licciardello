95974888bd849732d2227aa0db4606b8ef0f6ec1
Martin <martinlicciardello7@gmail.com>
2022-02-15 17:44:13 -0300

    first commit

---
+++ src/views/CharacterDetail/CharacterDetail.js
@@ -0,0 +1,49 @@
+import React, {useEffect,useState} from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import CharacterCard from '../../components/CharacterCard/CharacterCard';
+import './CharacterDetail.css';
+
+
+const CharacterDetail = () => {
+    const [character, setCharacter] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    let id = useParams();
+
+    let userID = id.id;
+
+    
+
+    useEffect(() => {
+      axios(`https://api.github.com/users/${userID}`).then((res) =>
+        setCharacter(res.data)
+      );
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 2000);
+    }, [userID]);
+  
+    return (
+
+      <div className="productDetail">
+  
+        {isLoading
+  
+          ?
+  
+          <p>Aguarde unos segundos</p>
+  
+          :
+  
+          <CharacterCard data={character} />
+  
+        }
+  
+      </div>
+  
+    );
+  };
+
+
+export default CharacterDetail
